bbbc0fba166b8c2b470cb558df4f76d56fcfd469
“BiankaFranke” <bianka_franke@hotmail.de>
2022-04-30 11:10:43 +0200

    JS Funktionen, Team-Name Button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,55 @@
 console.log('it works');
+
+// ==============Deklarien==============
+
+let CounterAnzeigeHome = document.querySelector('.CounterAnzeigeHome');
+let CounterAnzeigeAway = document.querySelector('.CounterAnzeigeAway');
+let Home= 0
+let Away = 0
+
+// ==============Home Funktionen==============
+
+function Home1() {
+    Home += 1;    // Home = Home + 1; 
+    CounterAnzeigeHome.innerHTML = Home;
+}
+
+function Home2() {
+    Home += 2;  // Home = Home + 2; 
+    CounterAnzeigeHome.innerHTML = Home;
+}
+
+function Home3() {
+    Home += 3; // Home = Home + 3; 
+    CounterAnzeigeHome.innerHTML = Home;
+}
+
+// ==============Awway Funktionen==============
+
+function Away1() {
+    Away += 1;    // Away = Away + 1; 
+
+    CounterAnzeigeAway.innerHTML = Away;
+}
+
+function Away2() {
+    Away += 2;     // Away = Away + 2; 
+    CounterAnzeigeAway.innerHTML = Away;
+}
+
+function Away3() {
+    Away += 3;     // Away = Away + 3; 
+    CounterAnzeigeAway.innerHTML = Away;
+}
+
+// ==============Reset Funktionen==============
+
+function resest() {
+    Home = 0;
+    Away = 0;
+    CounterAnzeigeHome.innerHTML = Home;
+    CounterAnzeigeAway.innerHTML = Away;
+}
+
+// ==============Team-Name Funktionen==============
+
